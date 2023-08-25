f44f415ebb910dcb0f0ae07e06d67eae5cde9876
Diego Rebora <bogacore@hotmail.com>
2023-08-25 17:27:19 -0300

    Primer comit, presentable

---
+++ scriptssuyaiweb.js
@@ -0,0 +1,19 @@
+function toggleMap(mapId) {
+    var map = document.getElementById(mapId);
+    if (map.style.display === "none") {
+      map.style.display = "block";
+     
+    } else {
+      map.style.display = "none";
+     
+    }
+  }
+
+  function toggleHorarios(horariosId) {
+    var horarios = document.getElementById(horariosId);
+    if (horarios.style.display === "none") {
+      horarios.style.display = "block";
+    } else {
+      horarios.style.display = "none";
+    }
+  }
