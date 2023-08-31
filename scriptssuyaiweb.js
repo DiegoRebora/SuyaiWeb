4894d75d8adf4c7a44eeaf69af2a3464d8c938d3
Diego Rebora <bogacore@hotmail.com>
2023-08-31 20:09:34 -0300

    Función unificada sin borrador

--- scriptssuyaiweb.js
+++ scriptssuyaiweb.js
@@ -1,22 +1,4 @@
-/*function toggleMap(mapId) {
-    let map = document.getElementById(mapId);
-    if (map.style.display === "none") {
-      map.style.display = "block";
-     
-    } else {
-      map.style.display = "none";
-     
-    }
-  }
 
-  function toggleHorarios(horariosId) {
-    let horarios = document.getElementById(horariosId);
-    if (horarios.style.display === "none") {
-      horarios.style.display = "block";
-    } else {
-      horarios.style.display = "none";
-    }
-  }*/
 
 function mostrar(variable) {
   let elemento = document.getElementById(variable);
@@ -25,3 +7,4 @@ function mostrar(variable) {
 } else {
   elemento.style.display = "none";
 } }
+
